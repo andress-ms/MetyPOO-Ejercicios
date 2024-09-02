c695e1f7389cdd6416a6956e1aa779f24cbb9f16
andress-ms <amartinezs@uamv.edu.ni>
2024-09-02 09:34:29 -0600

    se agregaron las clases Serie y Cuenta

--- Sesion4/src/main/java/Main.java
+++ Sesion4/src/main/java/Main.java
@@ -1,16 +1,27 @@
 import models.Circle;
 import models.Calculadora;
-
+import models.Cuenta;
+import models.Serie;
 import javax.swing.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
+
         JOptionPane.showMessageDialog(null, "Bienvenido");
         double radio, area;
         radio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio"));
         Circle circle = new Circle(radio);
         Calculadora calc = new Calculadora();
-        JOptionPane.showMessageDialog(null, "El area del circulo es "
-                + calc.calcularArea(circle));
+        JOptionPane.showMessageDialog(null, "El área del círculo es " + calc.calcularArea(circle));
+
+
+        Cuenta cuenta = new Cuenta("Silvio", 1000);
+        cuenta.ingresar(500);
+        cuenta.retirar(300);
+        JOptionPane.showMessageDialog(null, "Cuenta: " + cuenta.toString());
+
+
+        Serie serie = new Serie("Stranger Things", 4, "Ciencia Ficción", "Los Hermanos Duffer");
+        JOptionPane.showMessageDialog(null, "Serie: " + serie.toString());
     }
 }
---
+++ Sesion4/src/main/java/models/Cuenta.java
@@ -0,0 +1,55 @@
+package models;
+
+public class Cuenta {
+    private String titular;
+    private double cantidad;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(String titular) {
+        this.titular = titular;
+        this.cantidad = 0.0;
+    }
+
+    public Cuenta(String titular, double cantidad) {
+        this.titular = titular;
+        this.cantidad = cantidad < 0 ? 0 : cantidad;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String toString() {
+        return "Cuenta{" + "titular=" + titular +
+                ", cantidad=" + cantidad + '}';
+    }
+
+    public void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            this.cantidad += cantidad;
+        }
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad > 0) {
+            this.cantidad -= cantidad;
+            if (this.cantidad < 0) {
+                this.cantidad = 0;
+            }
+        }
+    }
+}
